6af87ce917558f30a4032dda56727e957cacb85f
Nikhil <Nking1247@users.noreply.github.com>
2021-11-15 15:03:50 -0600

    KindaWorks

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function changePage(){
-    if(document.getElementById("isLive").style.display == none)
-    document.getElementById("isLive").style.display = none;
+var date = new Date();
+var hour = d.getHours();
+
+
+
+function yesLive() {
+    document.getElementById("isLive").style.display = "block";
+    document.getElementById("isNotLive").style.display = "none";
+}
+
+function noLive() {
+    document.getElementById("isNotLive").style.display = "block";
+    document.getElementById("isLive").style.display = "none";
+}
+
+
+
+function buttonDisplay(){
+    if ( 1 == 1 ){
+        document.getElementById("yesButton").style.display = "block";
+        document.getElementById("isLive").style.display = "block";
+    document.getElementById("isNotLive").style.display = "none";
+    }else{
+        document.getElementById("noButton").style.display = "block";
+        document.getElementById("isNotLive").style.display = "block";
+    document.getElementById("isLive").style.display = "none";
+    }
 }
